096f493fc99c99c84a05186902e775c1402c647c
Connor Montgomery <connorthomasmontgomery@gmail.com>
2020-06-15 23:21:33 -0500

    update capabilities & info.plist

--- NPR/AppDelegate.swift
+++ NPR/AppDelegate.swift
@@ -7,16 +7,41 @@
 //
 
 import Cocoa
+import AVFoundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
+    let player = AVPlayer()
 
+    let statusBar = NSStatusBar.system
+    var statusBarItem:NSStatusItem!
+
+    var menu:NSMenu = NSMenu()
+    var isCurrentlyPlaying = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        guard let streamURL = URL.init(string: "https://kcurlive.umkc.edu/kcur893") else { return }
+        let streamItem = AVPlayerItem.init(url: streamURL)
+        player.replaceCurrentItem(with: streamItem)
+        player.play()
+        isCurrentlyPlaying = true
 
+        statusBarItem = statusBar.statusItem(withLength: -1)
+        statusBarItem.button?.title = "Hello, world!"
+        statusBarItem.menu = menu
+
+        menu.addItem(NSMenuItem.init(title: "Play / Pause", action: #selector(togglePlayPause), keyEquivalent: ""))
     }
 
+    @objc func togglePlayPause() {
+        if isCurrentlyPlaying {
+            player.pause()
+            isCurrentlyPlaying = false
+        } else {
+            player.play()
+            isCurrentlyPlaying = true
+        }
+    }
 }
-
