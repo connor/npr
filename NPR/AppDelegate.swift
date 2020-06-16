bef3a1672e954cf0f5ccd09a197e7bb44d12b5d5
Connor Montgomery <connorthomasmontgomery@gmail.com>
2020-06-15 23:44:40 -0500

    Bring to ending point of part 1.

--- NPR/AppDelegate.swift
+++ NPR/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  NPR
 //
-//  Created by Connor Montgomery on 6/8/20.
+//  Created by Connor Montgomery on 6/15/20.
 //  Copyright © 2020 Connor Montgomery. All rights reserved.
 //
 
@@ -20,6 +20,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var menu:NSMenu = NSMenu()
     var isCurrentlyPlaying = false
+    let playPauseIcon = NSMenuItem.init(title: "Pause", action: #selector(togglePlayPause), keyEquivalent: "")
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         guard let streamURL = URL.init(string: "https://kcurlive.umkc.edu/kcur893") else { return }
@@ -29,10 +30,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         isCurrentlyPlaying = true
 
         statusBarItem = statusBar.statusItem(withLength: -1)
-        statusBarItem.button?.title = "Hello, world!"
+        statusBarItem.button?.image = NSImage.init(named: "npr-icon")
         statusBarItem.menu = menu
 
-        menu.addItem(NSMenuItem.init(title: "Play / Pause", action: #selector(togglePlayPause), keyEquivalent: ""))
+        menu.addItem(playPauseIcon)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem.init(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+
+        updateMenuIcon()
+        updatePlayPauseButtonTitle()
+    }
+
+    @objc func quit() {
+        NSApp.terminate(nil)
     }
 
     @objc func togglePlayPause() {
@@ -43,5 +53,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             player.play()
             isCurrentlyPlaying = true
         }
+
+        updateMenuIcon()
+        updatePlayPauseButtonTitle()
+    }
+
+    func updateMenuIcon() -> Void {
+        let imageName = isCurrentlyPlaying ? "npr-icon" : "npr-icon-greyscale"
+        self.statusBarItem.button?.image = NSImage.init(named: imageName)
+    }
+
+    func updatePlayPauseButtonTitle() {
+        playPauseIcon.title = isCurrentlyPlaying ? "Pause" : "Play"
     }
 }
